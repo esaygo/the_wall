ec990d22bb5a5fc903ba56e8e7457be78f04f018
esaygo <elena.saygo@gmail.com>
2016-04-10 22:47:34 -0700

    comments show up

--- the_wall.php
+++ the_wall.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
+require_once('connection.php');
 ?>
 
 <!DOCTYPE html>
@@ -24,7 +25,6 @@ session_start();
   ?>
   </section>
 <div class="hline"></div>
-
 <div id="the_wall">
   <form class="pure-form" action="process.php" method="post">
     <input type='hidden' name="action" value="message">
@@ -35,7 +35,27 @@ session_start();
     <button type="submit" class="pure-button pure-input-1-2 pure-button-primary">Post a message</button>
 </form>
 </div>
+<div id="display_messages">
+  <?php
+  $query_count_msg = "SELECT count(*) FROM messages";
+  $count_msg = fetch_record($query_count_msg);
 
+  if($count_msg > 0) {
+    $query_get_msg = "SELECT first_name, last_name, users.created_at, message FROM users LEFT JOIN messages ON users.id = messages.users_id
+";
+    $messages = fetch_all($query_get_msg);
+    //var_dump($messages);
+    for($i=0; $i<count($messages); $i++){
+
+        echo '<div class="message"><li>' . $messages[$i]['first_name'] . " " . $messages[$i]['last_name'] . " - " . $messages[$i]['created_at'] ;
+        echo '<p>' . $messages[$i]['message'] . '</p></div><div class="hline"></div>  ';
+    }
+
+  }
+
+  ?>
+
+</div>
 
 </body>
 </html>
